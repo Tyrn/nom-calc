bb44392863de09e7442d661a2b44fd6a5b61b1ff
Tyrn <a@b.com>
2022-01-27 12:07:47 +0300

    rustyline introduced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use rustyline::config::{Config, EditMode};
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::builder().edit_mode(EditMode::Vi).build();
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::with_config(config);
+    if rl.load_history(".calc-history").is_err() {
+        eprintln!("No previous history.");
+    }
+
+    loop {
+        let readline = rl.readline("user> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+                rl.save_history(".calc-history").unwrap();
+                if line.len() > 0 {
+                    println!("{}", line);
+                }
+            }
+            Err(ReadlineError::Interrupted) => continue,
+            Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
 }
